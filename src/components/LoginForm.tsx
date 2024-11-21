6ad4920b7b085158c5d4d6593de2a4237bc76dbf
Paolo Pastacaldi <paolopastacaldi.dev@gmail.com>
2024-11-21 17:13:27 +0100

    add login form and logic

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = { email:username, password };
+    const response = await fetch('http://127.0.0.1:8000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    console.log("response", response);
+  };
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1 className='mb-4'>Login</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-2'>
+        <input className='border p-2 rounded-md' type="text" placeholder="Username" value={username} onChange={handleUsernameChange} />
+        <input className='border p-2 rounded-md' type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
